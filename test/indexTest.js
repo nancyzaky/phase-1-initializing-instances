f053dce6f3236d2d98334b02f5fc259331189bba
Nancy Zaky <nancyzki@yahoo.com>
2021-07-18 19:13:09 -0700

    commit tree 0b1b2c74b2205c030a055f5a8946524b00c68c9b

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,35 +1,40 @@
 describe("breakfast", () => {
   it("has a class of breakfast", () => {
     expect(new Breakfast()).to.be.an.instanceof(Breakfast);
-  })
+  });
   it("creates a new breakfast with the correct parameters", () => {
-    bfast = new Breakfast('eggs', 'juice')
-    expect(bfast.food).to.equal('eggs')
-    expect(bfast.drink).to.equal('juice')
-  })
-})
+    bfast = new Breakfast("eggs", "juice");
+    expect(bfast.food).to.equal("eggs");
+    expect(bfast.drink).to.equal("juice");
+  });
+});
 
 describe("lunch", () => {
   it("has a class of lunch", () => {
     expect(new Lunch()).to.be.an.instanceof(Lunch);
-  })
+  });
   it("creates a new lunch with the correct parameters", () => {
-    newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
-    expect(newlunch.salad).to.equal('side salad')
-    expect(newlunch.soup).to.equal('broccoli cheddar soup')
-    expect(newlunch.drink).to.equal('iced tea')
-  })
-})
+    newlunch = new Lunch("side salad", "broccoli cheddar soup", "iced tea");
+    expect(newlunch.salad).to.equal("side salad");
+    expect(newlunch.soup).to.equal("broccoli cheddar soup");
+    expect(newlunch.drink).to.equal("iced tea");
+  });
+});
 
 describe("dinner", () => {
   it("has a class of dinner", () => {
     expect(new Dinner()).to.be.an.instanceof(Dinner);
-  })
+  });
   it("creates a new dinner with the correct parameters", () => {
-    newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
-    expect(newdinner.salad).to.equal('balsamic salad')
-    expect(newdinner.soup).to.equal('consomme')
-    expect(newdinner.entree).to.equal('filet mignon')
-    expect(newdinner._dessert).to.equal('cheesecake')
-  })
-})
+    newdinner = new Dinner(
+      "balsamic salad",
+      "consomme",
+      "filet mignon",
+      "cheesecake"
+    );
+    expect(newdinner._salad).to.equal("balsamic salad");
+    expect(newdinner._soup).to.equal("consomme");
+    expect(newdinner._entree).to.equal("filet mignon");
+    expect(newdinner._dessert).to.equal("cheesecake");
+  });
+});
